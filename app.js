9ca0757fc203f887c8e9d13a534915abbeb1231e
Judy Tuan <judytuna@gmail.com>
2019-02-27 22:12:40 -0800

    Spike on calling out to Eventful, then hitting my API to save just one event.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const inquirer = require('inquirer');
 const fetch = require('node-fetch');
 
+const eventfulClient = require('./eventfulAPI');
+
 const app = {};
 
 app.startQuestion = (closeConnectionCallback) => {
@@ -10,6 +12,9 @@ app.startQuestion = (closeConnectionCallback) => {
     choices: [
       'Complete a sentence',
       'List all events',
+      'Input a new event',
+      'Call out to Eventful',
+      'Call out to Eventful then save the top event locally',
       'Exit'
     ],
     name:'action',
@@ -18,6 +23,9 @@ app.startQuestion = (closeConnectionCallback) => {
 
     if (res.action === 'Complete a sentence') app.completeSentence(continueCallback);
     if (res.action === 'List all events') app.getAllEvents(continueCallback);
+    if (res.action === 'Input a new event') app.inputNewEvent(continueCallback);
+    if (res.action === 'Call out to Eventful') app.callOutToEventful(continueCallback);
+    if (res.action === 'Call out to Eventful then save the top event locally') app.callOutToEventfulAndSave(continueCallback);
 
     if (res.action === 'Exit') {
       closeConnectionCallback();
@@ -37,6 +45,62 @@ app.getAllEvents = (continueCallback) => {
   fetch('http://localhost:3000/events')
     .then(res => res.json())
     .then(json => console.log(json));
+}
+
+app.inputNewEvent = (continueCallback) => {
+  fetch('http://localhost:3000/events', {method: 'POST'})
+    .then(res => res.json())
+    .then(json => console.log(json));
+}
+
+app.callOutToEventful = (continueCallback) => {
+  eventfulClient.searchEvents({
+    keywords: 'tango',
+    location: 'San Francisco',
+    date: "Next Week"
+  }, function(err, data){
+     if(err){
+       return console.error(err);
+     }
+     let resultEvents = data.search.events.event;
+     console.log('Received ' + data.search.total_items + ' events');
+     console.log('Event listings: ');
+     for ( let i =0 ; i < resultEvents.length; i++){
+       console.log("===========================================================")
+       console.log('title: ',resultEvents[i].title);
+       console.log('start_time: ',resultEvents[i].start_time);
+       console.log('venue_name: ',resultEvents[i].venue_name);
+       console.log('venue_address: ',resultEvents[i].venue_address);
+     }
+  });
+}
+
+app.callOutToEventfulAndSave = (continueCallback) => {
+  eventfulClient.searchEvents({
+    keywords: 'comedy',
+    location: 'San Francisco',
+    date: "Next Week"
+  }, function(err, data){
+     if(err){
+       return console.error(err);
+     }
+     let resultEvents = data.search.events.event;
+     console.log(resultEvents);
+     let oneEvent = resultEvents[0];
+     let postBody = JSON.stringify({
+      title: oneEvent.title,
+      city: oneEvent.city_name
+    })
+
+     fetch('http://localhost:3000/events', {
+       method: 'POST',
+       body: postBody,
+       headers: { 'Content-Type': 'application/json' }
+      })
+       .then(res => res.json())
+       .then(json => console.log(json));
+    }
+  )
 }
 
 module.exports = app;
--- eventfulAPI.js
+++ eventfulAPI.js
@@ -3,25 +3,24 @@ const eventful = require('eventful-node');
 const eventfulClient = new eventful.Client(eventfulKey);
 
 //sample search, try running it to see it in action
-eventfulClient.searchEvents({
-  keywords: 'tango',
-  location: 'San Francisco',
-  date: "Next Week"
-}, function(err, data){
-   if(err){
-     return console.error(err);
-   }
-   let resultEvents = data.search.events.event;
-   console.log('Received ' + data.search.total_items + ' events');
-   console.log('Event listings: ');
-   for ( let i =0 ; i < resultEvents.length; i++){
-     console.log("===========================================================")
-     console.log('title: ',resultEvents[i].title);
-     console.log('start_time: ',resultEvents[i].start_time);
-     console.log('venue_name: ',resultEvents[i].venue_name);
-     console.log('venue_address: ',resultEvents[i].venue_address);
-   }
-});
+// eventfulClient.searchEvents({
+//   keywords: 'tango',
+//   location: 'San Francisco',
+//   date: "Next Week"
+// }, function(err, data){
+//    if(err){
+//      return console.error(err);
+//    }
+//    let resultEvents = data.search.events.event;
+//    console.log('Received ' + data.search.total_items + ' events');
+//    console.log('Event listings: ');
+//    for ( let i =0 ; i < resultEvents.length; i++){
+//      console.log("===========================================================")
+//      console.log('title: ',resultEvents[i].title);
+//      console.log('start_time: ',resultEvents[i].start_time);
+//      console.log('venue_name: ',resultEvents[i].venue_name);
+//      console.log('venue_address: ',resultEvents[i].venue_address);
+//    }
+// });
 
-//export a custom function that searches via Eventful API, displays the results AND stores some of the data into MySQL
 module.exports = eventfulClient;
